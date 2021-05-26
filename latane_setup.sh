c6cfaa91ee1f3b3e54a3aaadf0dc68e86de6ebb2
Latane <1prlabu1@gmail.com>
2021-05-26 11:10:57 -0500

    macOS options updated

--- latane_setup.sh
+++ latane_setup.sh
@@ -1,4 +1,7 @@
 
+# remove some default keyboard shortcuts from macOS finder 
+# to enable other, better shortcuts
+defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Computer" nil
 
 git config --global user.name "Latane Bullock"
 git config --global user.email 1prlabu1@gmail.com
