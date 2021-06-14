694fcb526cb2c13cb162933f5d45df73538f8c7a
Latane <1prlabu1@gmail.com>
2021-06-14 12:44:30 -0500

    add gitignores, also install homebrew

--- latane_setup.sh
+++ latane_setup.sh
@@ -5,3 +5,17 @@ defaults write com.apple.Finder NSUserKeyEquivalents -dict-add "Computer" nil
 
 git config --global user.name "Latane Bullock"
 git config --global user.email 1prlabu1@gmail.com
+
+# ignore macOS temporary files
+echo ".DS_STORE" >> ~/.gitignore 
+
+# global git ignore
+git config --global core.excludesfile ~/.gitignore 
+
+
+# install homebrew, install packages
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install xpdf # renderer for exporting matlab figures
+brew install ghostscript # renderer for exporting matlab figures
+
